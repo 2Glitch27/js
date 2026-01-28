f0c9197a25159ee5f72d5a7d651a132981730ea7
2Glitch27 <240958284+2Glitch27@users.noreply.github.com>
2026-01-28 21:06:38 +0300

    12

---
+++ main.js
@@ -0,0 +1,19 @@
+// console.log("Привет");
+// let x = ' Привет мир';
+// console.log(x);
+// let valuePrompt = prompt();
+// alert(valuePrompt);
+// console.log(+valuePrompt)
+
+
+// valuePrompt = +prompt('Введите ваше число');
+// alert(valuePrompt ** 2);
+
+// let x = +prompt('Введите ваше 1 число');
+// let y = +prompt('Введите ваше 2 число');
+// let average = (x + y) / 2;
+// alert(average)
+
+let x = +prompt('Введите длину стороны квадрата');
+let s = x ** 2;
+alert( " Площадь квадрата равна " +s);
