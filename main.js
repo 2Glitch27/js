7b5d9405347f69b425c44c87fdff7dd059c2d4af
2Glitch27 <chiter1019@yandex.ru>
2026-02-02 12:06:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,45 @@
 // let average = (x + y) / 2;
 // alert(average)
 
-let x = +prompt('Введите длину стороны квадрата');
-let s = x ** 2;
-alert( " Площадь квадрата равна " +s);
+// let x = +prompt('Введите длину стороны квадрата');
+// let s = x ** 2;
+// alert( " Площадь квадрата равна " +s);
+
+
+
+
+
+
+// let nick = prompt('Введите ваше имя');
+// alert( "Привет, " + nick );
+
+// let yearOfBirth = +prompt('Введите год своего рождения');
+// const ourYear = 2026;
+// alert(ourYear - yearOfBirth);
+
+// let squareLength = +prompt('Введите длину стороны квадрата');
+// alert (squareLength * 4);
+
+// let circleRadius = +prompt('Введите радиус окружности');
+// const PI = 3.14;
+// alert((circleRadius ** 2) * PI);
+
+
+// let distance = +prompt('Введите расстояние в км');
+// let time = +prompt('Введите часы ');
+// alert (distance / time);
+
+// let dollars = +prompt('Введите доллары');
+// const exchange = 0.84;
+// alert( dollars * exchange);
+
+// let flashDriveCapacity = +prompt('Введите объем флешки');
+// let transformation = flashDriveCapacity * 1024;
+// alert(Math.floor(transformation/820));
+
+
+// let wallet = +prompt('Введите сумму в кошельке');
+// let price = +prompt('Введите цену');
+// alert (wallet - price);
+
+
